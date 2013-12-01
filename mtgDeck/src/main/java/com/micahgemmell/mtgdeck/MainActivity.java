35922cea5f39fcde601048bbc68265a338c98931
sonicemerald <sonicemerald@gmail.com>
2013-11-30 19:14:17 -0800

    implemented CardView Fragment, item clicked loads image of card

--- mtgDeck/src/main/java/com/micahgemmell/mtgdeck/MainActivity.java
+++ mtgDeck/src/main/java/com/micahgemmell/mtgdeck/MainActivity.java
@@ -3,6 +3,8 @@ package com.micahgemmell.mtgdeck;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,7 +25,10 @@ import java.util.List;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
-public class MainActivity extends Activity {
+import com.loopj.android.http.BinaryHttpResponseHandler;
+import com.micahgemmell.mtgdeck.Card;
+
+public class MainActivity extends Activity implements listView_F.OnCardView {
     public String set = "THS";
     public String jsonmtg = "http://mtgjson.com/json/".concat(set).concat(".json");
 
@@ -31,10 +36,10 @@ public class MainActivity extends Activity {
     ListView listView;
     ArrayAdapter<Card> adapter;
     JSONArray jArray;
-    JSONArray aArray;
-    String part2;
     String tempUrl;
 
+    listView_F listView_f;
+    cardView_F cardView_f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +49,23 @@ public class MainActivity extends Activity {
         // Setup card Container
         cards = new ArrayList<Card>();
 
-        // Setup Listview for cards
-        listView = (ListView) findViewById(R.id.listView);
+       listView_f = new listView_F(cards);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, listView_f)
+                    .commit();
+        }
+
+        // Setup Listview for cards (now done in fragment)
+     //   listView = (ListView) findViewById(R.id.listView);
+
         // NOTE: It is OK to use a SimpleObject, with an android.R.layout.simple_list_item_1 because
         // SimpleObject has a toString method declared, which the adapter calls to generate the
         // ListView text.
-        adapter = new ArrayAdapter<Card>(this, android.R.layout.simple_list_item_1, cards);
-        listView.setAdapter(adapter);
+
+     //   adapter = new ArrayAdapter<Card>(this, android.R.layout.simple_list_item_1, cards);
+     //   listView.setAdapter(adapter);
 
         // Now that the adapter is setup, if we need to update the listView we will use adapter.add
         // instead of items.add, the adapter will take care of adding to the host container.
@@ -96,6 +111,7 @@ public class MainActivity extends Activity {
                                         card.setManacost(0);
                                     }
                                 card.setImageName(jObject.getString("imageName"));
+                                //card.setSet(jObject.getString("set"));
 
                         // Remember there are other items in the JSON Object, and they are of other
                         // Types, so you might want to switch based on type and create an object
@@ -114,7 +130,8 @@ public class MainActivity extends Activity {
 
                         // Add an Item to the Adapter, which will add it to the items List, and
                         // update the List View
-                        adapter.add(card);
+                        listView_f.adapter.add(card);
+                        //adapter.add(card);
                                 }
                              } catch (JSONException e) {
                                         Log.d("JSON Parse", e.toString());
@@ -128,148 +145,30 @@ public class MainActivity extends Activity {
         }  );
     }
 
-    /*    if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+
+    @Override
+    public void onCardViewUpdate(int position){
+    cardView_f = new cardView_F(cards.get(position));
+        getFragmentManager().beginTransaction()
+                    .replace(R.id.container, cardView_f)
+                    .addToBackStack("CardView Back")
                     .commit();
-        }
-    }
-    /**
-     * A placeholder fragment containing a simple view.
-     *//*
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+        String image = cards.get(position).getImageName();
+        String set = cards.get(position).getSet();
+        String url = "http://mtgimage.com/set/".concat(set).concat("/").concat(image).concat(".jpg");
+        Log.d("tag", url);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }*/
-
-    /**
-     * Created by MXPS on 11/29/13.
-     */
-
-    public class Card {
-        public String set = "set";
-        public String name = "name";
-        public String imageName = "iname";
-        public String type = "type";
-        public String subtype = "stype";
-        public String color = "color";
-        public int manacost = 0;
-        public String cardtext = "cardtext";
-        public String flavortext = "flavortext";
-
-        public Card() {
-        }
-
-        public Card(String set, String name, String type, String subtype, String color, int manacost, String cardtext, String flavortext, String imageName) {
-            this.set = set;
-            this.name = name;
-            this.imageName = imageName;
-            this.type = type;
-            this.color = color;
-            this.subtype = subtype;
-            this.manacost = manacost;
-            this.cardtext = cardtext;
-            this.flavortext = flavortext;
-        }
-
-        @Override
-        public String toString() {
-            // Example of a String Builder like we talked about in class.
-            StringBuilder builder = new StringBuilder();
-            return builder.append(set).append(".")
-                    .append(name).append(", ")
-                    .append(type).append(", ")
-                    .append(subtype).append(", ")
-                    .append(color).append(", ")
-                    .append(manacost).append(", ")
-                    .append(cardtext).append(",")
-                    .append(flavortext).append(", ")
-                    .append(imageName).append(",")
-                    .toString();
-        }
-
-        public String getSet() {
-            return set;
-        }
-
-        public void setSet(String set) {
-            this.set = set;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getImageName() {
-            return imageName;
-        }
-
-        public void setImageName(String imageName) {
-            this.imageName = imageName;
-        }
-
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-
-        public String getSubtype() {
-            return subtype;
-        }
-
-        public void setSubtype(String subtype) {
-            this.subtype = subtype;
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-        public void setColor(String color) {
-            this.color = color;
-        }
-
-        public int getManacost() {
-            return manacost;
-        }
-
-        public void setManacost(int manacost) {
-            this.manacost = manacost;
-        }
-
-        public String getCardtext() {
-            return cardtext;
-        }
-
-        public void setCardtext(String cardtext) {
-            this.cardtext = cardtext;
-        }
-
-        public String getFlavortext() {
-            return flavortext;
-        }
-
-        public void setFlavortext(String flavortext) {
-            this.flavortext = flavortext;
-        }
+        AsyncHttpClient client = new AsyncHttpClient();
+        String[] allowedContentTypes = new String[] { "image/jpeg" };
+        client.get(url, new BinaryHttpResponseHandler(allowedContentTypes) {
+            @Override
+            public void onSuccess(byte[] fileData) {
+                Bitmap imageBitmap = BitmapFactory.decodeByteArray(fileData, 0, fileData.length);
+                if (cardView_f != null)
+                    cardView_f.setImageView(imageBitmap);
+            }
+        });
 
     }
-
 }
