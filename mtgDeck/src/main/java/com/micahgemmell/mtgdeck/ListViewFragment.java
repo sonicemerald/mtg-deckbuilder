c59acde9c6648a4c55e2f88d700cce0b9301cdff
sonicemerald <sonicemerald@gmail.com>
2013-12-07 21:55:51 -0800

    new master

--- mtgDeck/src/main/java/com/micahgemmell/mtgdeck/ListViewFragment.java
+++ mtgDeck/src/main/java/com/micahgemmell/mtgdeck/ListViewFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import java.util.List;
@@ -24,6 +25,7 @@ public class ListViewFragment
     Context context;
     ListView listView;
     OnCardView mListener;
+
 
     public ListViewFragment(List<Card> card)
     {
